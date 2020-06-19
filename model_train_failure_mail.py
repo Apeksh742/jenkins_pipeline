ce8519673015987507d8edafac05f6c75e70face
Apeksh Agarwal <59861221+Apeksh742@users.noreply.github.com>
2020-06-19 14:11:48 +0530

    initial commit

---
+++ model_train_failure_mail.py
@@ -0,0 +1,26 @@
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+sender_email = "Apekshagarwal369@gmail.com"
+sender_password = # "rj14sz7820"
+receiver_email = "Apekshagarwal8@gmail.com"
+s.login(sender_email, sender_password ) 
+  
+# message to be sent 
+message = '''\
+Subject: App deployment failed
+Hello, 
+				Developer this is an email regarding your last commit. It seems that your app/webserver is not working properly please check it once and recommit.
+			THANK YOU'''
+  
+# sending the mail 
+s.sendmail(sender_email,receiver_email , message) 
+  
+# terminating the session 
+s.quit() 
